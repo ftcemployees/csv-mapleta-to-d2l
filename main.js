1ecd66a10ca2a8105213dac4611565401fa97ab0
Joshua McKinney <mckinneyj@byui.edu>
2016-08-12 21:35:59 -0600

    cleaned up the code a bit

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 /*jslint plusplus: true, browser: true, devel: true */
 /*global FileReaderJS, csvMapleTAToD2L, download*/
-var fileInfo, assignmentNameText, outOf, colLength, parseCol, colNames;
 
 (function () {
-
     "use strict";
+
+    var fileInfo, colLength, parseCol, colNames, options;
 
     /***************************************************/
     /****************** ERROR CHECKING *****************/
@@ -13,44 +13,36 @@ var fileInfo, assignmentNameText, outOf, colLength, parseCol, colNames;
         var errorPara = document.querySelector("#errorMessage"),
             errorMessage;
 
-	errorPara.innerHTML = "";
-	
-	if (typeof errors == "string") {
-	    errorMessage = document.createTextNode(errors);
-	} else {
-	    errorMessage = document.createTextNode(errors.join('\n'));
-	}
+        errorPara.innerHTML = "";
+
+        if (typeof errors === "string") {
+            errorMessage = document.createTextNode(errors);
+        } else {
+            errorMessage = document.createTextNode(errors.join('\n'));
+        }
 
         errorPara.appendChild(errorMessage);
-	errorPara.style.opacity = 1;
-	window.setTimeout(function() {
-	    errorPara.style.opacity = 0;
-	}, 5000);
+        errorPara.style.opacity = 1;
+        window.setTimeout(function () {
+            errorPara.style.opacity = 0;
+        }, 5000);
     }
 
     /*Validate the inputs*/
     function validateGo() {
-	var inputs = document.getElementsByTagName('input'),
-	    index = 0,
-	    atLeastOneChecked = false;
-	
-	for (; index < inputs.length; ++index) {
-	    if (inputs[index].type.toLowerCase() == "checkbox" &&
-		inputs[index].checked == true) {
-		atLeastOneChecked = true;
-	    }
-	}
+        var inputs = document.getElementsByTagName('input'),
+            index,
+            atLeastOneChecked = false;
 
-	if (!atLeastOneChecked) {
-	    throw new Error("At least one grade item must be included.");
-	}
-    }
+        for (index = 0; index < inputs.length; ++index) {
+            if (inputs[index].type.toLowerCase() === "checkbox" && inputs[index].checked === true) {
+                atLeastOneChecked = true;
+            }
+        }
 
-    /*Show to next part of the form*/
-    function showGo() {
-        var classList = document.querySelector('#go').classList;
-	// Should this be changed to toggle the button on or off? Might not be necessary.
-        classList.add('on');
+        if (!atLeastOneChecked) {
+            throw new Error("At least one grade item must be included.");
+        }
     }
 
     /*Load all the options for the conversion Brightspace CSV gradesheet.*/
@@ -58,138 +50,143 @@ var fileInfo, assignmentNameText, outOf, colLength, parseCol, colNames;
         var arrExport = [],
             i,
             objExport,
-	    table = document.getElementsByTagName("table")[0],
-	    queryString,
-	    length = document.querySelectorAll("table tr").length;
+            table = document.getElementsByTagName("table")[0],
+            queryString,
+            length = document.querySelectorAll("table tr").length;
 
         for (i = 1; i < length; i++) {
-	    queryString = "table tr:nth-of-type(" + (i + 1) + ") ";
-	    if (document.querySelectorAll(queryString + ":checked").length > 0) {
-		objExport = {
-                    nameOld:        document.querySelector(queryString + "th").innerHTML,
-                    nameNew:        document.querySelector(queryString + "td:nth-of-type(1) input").value,
+            queryString = "table tr:nth-of-type(" + (i + 1) + ") ";
+            if (document.querySelectorAll(queryString + ":checked").length > 0) {
+                objExport = {
+                    nameOld: document.querySelector(queryString + "th").innerHTML,
+                    nameNew: document.querySelector(queryString + "td:nth-of-type(1) input").value,
                     pointsPossible: parseFloat(document.querySelector(queryString + "td:nth-of-type(2) input").value)
-		};
-		
-		arrExport.push(objExport);
-	    }
+                };
+
+                arrExport.push(objExport);
+            }
         }
 
-        console.log(arrExport)
-        
+        console.log(arrExport);
+
         return arrExport;
     }
 
-    var options = {
+    function onLoadFileEnd(e, file) {
+        console.log(e.target.result);
+        console.log(file);
+        fileInfo = {
+            text: e.target.result,
+            name: file.name,
+            nameNoExtention: file.extra.nameNoExtension,
+            mimeType: file.type
+        };
+
+        try {
+            parseCol = csvMapleTAToD2L.parse(e.target.result);
+        } catch (er) {
+            displayErr(er.message);
+        }
+
+        colNames = csvMapleTAToD2L.getGradeColNames(parseCol);
+
+        var columnNameContainer = document.querySelector("#columnNameContainer"),
+            table = document.createElement("table"),
+            row,
+            cell,
+            i,
+            labelContainer,
+            label1,
+            label2,
+            label1TextNode,
+            assignmentNameInput,
+            label2TextNode,
+            outOfInput,
+            checkBox;
+
+        columnNameContainer.innerHTML = '';
+
+        colLength = colNames.length;
+
+        // Heading Cells 'Blank Cell', 'Bright Space Name', 'Points Possible'
+        row = document.createElement("tr");
+        cell = document.createElement("th");
+        row.appendChild(cell);
+
+        cell = document.createElement("th");
+        cell.innerHTML = "Bright Space Name";
+        row.appendChild(cell);
+
+        cell = document.createElement("th");
+        cell.innerHTML = "Points Possible";
+        row.appendChild(cell);
+
+        cell = document.createElement("th");
+        cell.innerHTML = "Include?";
+        row.appendChild(cell);
+
+        table.appendChild(row);
+
+        /*Dynamically create inputs for each
+        gradable column on the CSV import*/
+        for (i = 0; i < colLength; i++) {
+            // Name of assignment
+            row = document.createElement("tr");
+            cell = document.createElement("th");
+            cell.innerHTML = colNames[i];
+            row.appendChild(cell);
+
+            // Input for brightspace name
+            assignmentNameInput = document.createElement("input");
+            assignmentNameInput.id = "assignmentName";
+            assignmentNameInput.type = "text";
+            assignmentNameInput.className = "columnIDLabel1" + [i];
+            //assignmentNameInput.oninput = showGo;
+
+            cell = document.createElement("td");
+            cell.appendChild(assignmentNameInput);
+            row.appendChild(cell);
+
+            // Input for points possible
+            outOfInput = document.createElement("input");
+            outOfInput.id = "outOf";
+            outOfInput.type = "number";
+            outOfInput.className = "columnIDLabel2" + [i];
+            //outOfInput.oninput = showGo;
+
+            cell = document.createElement("td");
+            cell.appendChild(outOfInput);
+            row.appendChild(cell);
+
+            // Input for including the grade item
+            checkBox = document.createElement("input");
+            checkBox.type = "checkbox";
+            checkBox.checked = true;
+
+            cell = document.createElement("td");
+            cell.appendChild(checkBox);
+            row.appendChild(cell);
+
+            // Append the row to the table
+            table.appendChild(row);
+        }
+
+        //add in the table guts
+        columnNameContainer.appendChild(table);
+        //add in the file name so the user can see what file they picked
+        document.querySelector('#filename').innerHTML = "Filename: " + fileInfo.name;
+        //show the rest of the ui
+        document.querySelector('#options').classList.add('on');
+    }
+
+    /***************************************************/
+    /********************** START **********************/
+    /***************************************************/
+    options = {
         readAsDefault: "Text",
         dragClass: "dropping",
         on: {
-            loadend: function (e, file) {
-                console.log(e.target.result);
-                console.log(file);
-                fileInfo = {
-                    text: e.target.result,
-                    name: file.name,
-                    nameNoExtention: file.extra.nameNoExtension,
-                    mimeType: file.type
-                };
-
-                try {
-		    parseCol = csvMapleTAToD2L.parse(e.target.result);
-		} catch (e) {
-		    displayErr(e.message);
-		}
-		
-                colNames = csvMapleTAToD2L.getGradeColNames(parseCol);
-
-                var columnNameContainer = document.querySelector("#columnNameContainer"),
-                    table = document.createElement("table"),
-                    row,
-                    cell,
-                    i,
-                    labelContainer,
-                    label1,
-                    label2,
-                    label1TextNode,
-                    assignmentNameInput,
-                    label2TextNode,
-                    outOfInput,
-                    checkBox;
-
-                columnNameContainer.innerHTML = '';
-                
-                colLength = colNames.length;
-                
-                // Heading Cells 'Blank Cell', 'Bright Space Name', 'Points Possible'
-                row = document.createElement("tr");
-                cell = document.createElement("th");
-                row.appendChild(cell);
-                
-                cell = document.createElement("th");
-                cell.innerHTML = "Bright Space Name";
-                row.appendChild(cell);
-                
-                cell = document.createElement("th");
-                cell.innerHTML = "Points Possible";
-                row.appendChild(cell);
-                
-                cell = document.createElement("th");
-                cell.innerHTML = "Include?";
-                row.appendChild(cell);
-                
-                table.appendChild(row);
-                
-                /*Dynamically create inputs for each
-                gradable column on the CSV import*/
-                for (i = 0; i < colLength; i++) {
-                    // Name of assignment
-                    row = document.createElement("tr");
-                    cell = document.createElement("th");
-                    cell.innerHTML = colNames[i];
-                    row.appendChild(cell);
-                    
-                    // Input for brightspace name
-                    assignmentNameInput = document.createElement("input");
-                    assignmentNameInput.id = "assignmentName";
-                    assignmentNameInput.type = "text";
-                    assignmentNameInput.className = "columnIDLabel1" + [i];
-                    //assignmentNameInput.oninput = showGo;
-                    
-                    cell = document.createElement("td");
-                    cell.appendChild(assignmentNameInput);
-                    row.appendChild(cell);
-
-                    // Input for points possible
-                    outOfInput = document.createElement("input");
-                    outOfInput.id = "outOf";
-                    outOfInput.type = "number";
-                    outOfInput.className = "columnIDLabel2" + [i];
-                    //outOfInput.oninput = showGo;
-
-                    cell = document.createElement("td");
-                    cell.appendChild(outOfInput);
-                    row.appendChild(cell);
-                    
-                    // Input for including the grade item
-                    checkBox = document.createElement("input");
-                    checkBox.type = "checkbox";
-                    checkBox.checked = true;
-		    
-                    cell = document.createElement("td");
-                    cell.appendChild(checkBox);
-                    row.appendChild(cell);
-                    
-                    // Append the row to the table
-                    table.appendChild(row);
-                }
-
-                columnNameContainer.appendChild(table);
-                
-                document.querySelector('#options').classList.add('on');
-                document.querySelector('#filename').innerHTML = "Filename: " + fileInfo.name;
-
-		showGo();
-            }
+            loadend: onLoadFileEnd
         }
     };
 
@@ -208,17 +205,17 @@ var fileInfo, assignmentNameText, outOf, colLength, parseCol, colNames;
         console.log("time:", time);
 
         //set the global values
-	try {
-	    validateGo();
-	    arrExport = getOptions();
+        try {
+            validateGo();
+            arrExport = getOptions();
 
             //run the code
             console.log("fileInfo.text:", fileInfo.text);
             converted = csvMapleTAToD2L.convert(parseCol, arrExport);
             download(converted, "converted_" + fileInfo.nameNoExtention + '_' + time + '.csv', fileInfo.mimeType);
-	} catch (e) {
-	    displayErr(e.message);
-	}
+        } catch (e) {
+            displayErr(e.message);
+        }
     };
 
 }());
