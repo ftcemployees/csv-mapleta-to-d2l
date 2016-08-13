99c938b316b790b4505ada4fbb83893d05a5eb9e
Joshua McKinney <mckinneyj@byui.edu>
2016-08-13 10:29:45 -0600

    it pretends to run the valence command

---
+++ gradeItems.js
@@ -0,0 +1,638 @@
+/*jslint node: true */
+
+module.exports = [{
+    "MaxPoints": 10,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": false,
+    "GradeSchemeId": null,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 12858,
+    "Name": "SCORM Test",
+    "ShortName": "Homework 02",
+    "GradeType": "Numeric",
+    "CategoryId": 6355,
+    "Description": {
+        "Text": "",
+        "Html": "<p>this is the Test SCORM</p>"
+    },
+    "Weight": 102,
+    "AssociatedTool": {
+        "ToolId": 78000,
+        "ToolItemId": 62
+    }
+}, {
+    "MaxPoints": 10,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": false,
+    "GradeSchemeId": null,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 15916,
+    "Name": "Test grade item",
+    "ShortName": "Homework 01",
+    "GradeType": "Numeric",
+    "CategoryId": 6355,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 10,
+    "AssociatedTool": {
+        "ToolId": 51000,
+        "ToolItemId": 15286
+    }
+}, {
+    "MaxPoints": 10,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": false,
+    "GradeSchemeId": null,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 17834,
+    "Name": "group 1 board",
+    "ShortName": "",
+    "GradeType": "Numeric",
+    "CategoryId": 0,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 10,
+    "AssociatedTool": {
+        "ToolId": 3000,
+        "ToolItemId": 18149
+    }
+}, {
+    "MaxPoints": 10,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": false,
+    "GradeSchemeId": null,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 17835,
+    "Name": "group 2 board",
+    "ShortName": "",
+    "GradeType": "Numeric",
+    "CategoryId": 0,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 10,
+    "AssociatedTool": {
+        "ToolId": 3000,
+        "ToolItemId": 18150
+    }
+}, {
+    "MaxPoints": 10,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": false,
+    "GradeSchemeId": null,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 17836,
+    "Name": "group 3 board",
+    "ShortName": "",
+    "GradeType": "Numeric",
+    "CategoryId": 0,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 10,
+    "AssociatedTool": {
+        "ToolId": 3000,
+        "ToolItemId": 18151
+    }
+}, {
+    "MaxPoints": 10,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": false,
+    "GradeSchemeId": null,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 36222,
+    "Name": "test json 2",
+    "ShortName": "Homework 04",
+    "GradeType": "Numeric",
+    "CategoryId": 6355,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 10,
+    "AssociatedTool": {
+        "ToolId": 78000,
+        "ToolItemId": 349
+    }
+}, {
+    "MaxPoints": 10,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": false,
+    "GradeSchemeId": 0,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 36689,
+    "Name": "json3",
+    "ShortName": "",
+    "GradeType": "Numeric",
+    "CategoryId": 0,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 10,
+    "AssociatedTool": {
+        "ToolId": 78000,
+        "ToolItemId": 351
+    }
+}, {
+    "MaxPoints": 100,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": true,
+    "GradeSchemeId": null,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 36789,
+    "Name": "testStorylineTitleLesson",
+    "ShortName": "",
+    "GradeType": "Numeric",
+    "CategoryId": 0,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 0,
+    "AssociatedTool": {
+        "ToolId": 78000,
+        "ToolItemId": 353
+    }
+}, {
+    "MaxPoints": 10,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": false,
+    "GradeSchemeId": null,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 38030,
+    "Name": "Lesson 01 Review",
+    "ShortName": "",
+    "GradeType": "Numeric",
+    "CategoryId": 0,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 10,
+    "AssociatedTool": {
+        "ToolId": 78000,
+        "ToolItemId": 355
+    }
+}, {
+    "MaxPoints": 10,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": false,
+    "GradeSchemeId": null,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 38031,
+    "Name": "Lesson 02 Review",
+    "ShortName": "",
+    "GradeType": "Numeric",
+    "CategoryId": 0,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 10,
+    "AssociatedTool": {
+        "ToolId": 78000,
+        "ToolItemId": 357
+    }
+}, {
+    "MaxPoints": 100,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": true,
+    "GradeSchemeId": null,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 38572,
+    "Name": "Small Storyline lesson",
+    "ShortName": "",
+    "GradeType": "Numeric",
+    "CategoryId": 0,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 0,
+    "AssociatedTool": {
+        "ToolId": 78000,
+        "ToolItemId": 359
+    }
+}, {
+    "MaxPoints": 10,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": false,
+    "GradeSchemeId": null,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 38635,
+    "Name": "Test Rubric",
+    "ShortName": "",
+    "GradeType": "Numeric",
+    "CategoryId": 0,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 10,
+    "AssociatedTool": null
+}, {
+    "MaxPoints": 10,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": false,
+    "GradeSchemeId": null,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 59151,
+    "Name": "hasGrade",
+    "ShortName": "Test 02",
+    "GradeType": "Numeric",
+    "CategoryId": 187259,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 10,
+    "AssociatedTool": {
+        "ToolId": 51000,
+        "ToolItemId": 51269
+    }
+}, {
+    "MaxPoints": 11,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": false,
+    "GradeSchemeId": null,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 149804,
+    "Name": "Small Storyline test",
+    "ShortName": "",
+    "GradeType": "Numeric",
+    "CategoryId": 0,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 10,
+    "AssociatedTool": {
+        "ToolId": 78000,
+        "ToolItemId": 877
+    }
+}, {
+    "MaxPoints": 100,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": true,
+    "GradeSchemeId": null,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 149808,
+    "Name": "testStorylineTitleLesson",
+    "ShortName": "",
+    "GradeType": "Numeric",
+    "CategoryId": 0,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 0,
+    "AssociatedTool": null
+}, {
+    "MaxPoints": 10,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": false,
+    "GradeSchemeId": null,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 149809,
+    "Name": "Small Storyline test Debug",
+    "ShortName": "",
+    "GradeType": "Numeric",
+    "CategoryId": 0,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 10,
+    "AssociatedTool": {
+        "ToolId": 78000,
+        "ToolItemId": 879
+    }
+}, {
+    "MaxPoints": 10,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": false,
+    "GradeSchemeId": null,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 150309,
+    "Name": "SaveAScore",
+    "ShortName": "",
+    "GradeType": "Numeric",
+    "CategoryId": 0,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 10,
+    "AssociatedTool": {
+        "ToolId": 78000,
+        "ToolItemId": 881
+    }
+}, {
+    "MaxPoints": 10,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": false,
+    "GradeSchemeId": null,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 150314,
+    "Name": "SaveAScoreNoSequ",
+    "ShortName": "",
+    "GradeType": "Numeric",
+    "CategoryId": 0,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 10,
+    "AssociatedTool": {
+        "ToolId": 78000,
+        "ToolItemId": 883
+    }
+}, {
+    "MaxPoints": 10,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": false,
+    "GradeSchemeId": null,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 157150,
+    "Name": "Suspend Data Template Grade",
+    "ShortName": "",
+    "GradeType": "Numeric",
+    "CategoryId": 0,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 10,
+    "AssociatedTool": {
+        "ToolId": 78000,
+        "ToolItemId": 885
+    }
+}, {
+    "MaxPoints": 10,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": false,
+    "GradeSchemeId": null,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 167165,
+    "Name": "BOMSCORMSessionDay",
+    "ShortName": "Test 01",
+    "GradeType": "Numeric",
+    "CategoryId": 187259,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 10,
+    "AssociatedTool": {
+        "ToolId": 78000,
+        "ToolItemId": 887
+    }
+}, {
+    "MaxPoints": 20,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": false,
+    "GradeSchemeId": null,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 183189,
+    "Name": "Testing category Max Points",
+    "ShortName": "Homework 03",
+    "GradeType": "Numeric",
+    "CategoryId": 6355,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 10,
+    "AssociatedTool": null
+}, {
+    "MaxPoints": 10,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": false,
+    "GradeSchemeId": null,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 184281,
+    "Name": "Word Count",
+    "ShortName": "Test 03",
+    "GradeType": "Numeric",
+    "CategoryId": 187259,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 10,
+    "AssociatedTool": {
+        "ToolId": 51000,
+        "ToolItemId": 7223
+    }
+}, {
+    "Id": 187268,
+    "Name": "subCat",
+    "ShortName": "",
+    "GradeType": "Formula",
+    "CategoryId": 0,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 10,
+    "AssociatedTool": null
+}, {
+    "Id": 187271,
+    "Name": "other subcat",
+    "ShortName": "",
+    "GradeType": "Calculated",
+    "CategoryId": 0,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 10,
+    "AssociatedTool": null
+}, {
+    "MaxPoints": 66,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": false,
+    "GradeSchemeId": 6419,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/6419",
+    "Id": 218026,
+    "Name": "somethingCool",
+    "ShortName": "",
+    "GradeType": "Numeric",
+    "CategoryId": 0,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 10,
+    "AssociatedTool": {
+        "ToolId": 78000,
+        "ToolItemId": 891
+    }
+}, {
+    "MaxPoints": 100,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": true,
+    "GradeSchemeId": null,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 243226,
+    "Name": "Book of Mormon Reading Assignment",
+    "ShortName": "",
+    "GradeType": "Numeric",
+    "CategoryId": 0,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 0,
+    "AssociatedTool": {
+        "ToolId": 78000,
+        "ToolItemId": 1054
+    }
+}, {
+    "MaxPoints": 10,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": false,
+    "GradeSchemeId": null,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 250590,
+    "Name": "text regex grade item",
+    "ShortName": "Homework 05",
+    "GradeType": "Numeric",
+    "CategoryId": 6355,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 10,
+    "AssociatedTool": {
+        "ToolId": 51000,
+        "ToolItemId": 6826
+    }
+}, {
+    "MaxPoints": 10,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": false,
+    "GradeSchemeId": 6419,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/6419",
+    "Id": 267009,
+    "Name": "drop down",
+    "ShortName": "",
+    "GradeType": "SelectBox",
+    "CategoryId": 0,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 10,
+    "AssociatedTool": null
+}, {
+    "MaxPoints": 10,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": false,
+    "GradeSchemeId": null,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 335874,
+    "Name": "I am awesome",
+    "ShortName": "",
+    "GradeType": "Numeric",
+    "CategoryId": 0,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 10,
+    "AssociatedTool": {
+        "ToolId": 51000,
+        "ToolItemId": 7606
+    }
+}, {
+    "MaxPoints": 10,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": false,
+    "GradeSchemeId": null,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 535445,
+    "Name": "auto grade setting",
+    "ShortName": "",
+    "GradeType": "Numeric",
+    "CategoryId": 0,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 10,
+    "AssociatedTool": null
+}, {
+    "MaxPoints": 10,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": false,
+    "GradeSchemeId": null,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 537879,
+    "Name": "Test auto export",
+    "ShortName": "",
+    "GradeType": "Numeric",
+    "CategoryId": 0,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 10,
+    "AssociatedTool": {
+        "ToolId": 51000,
+        "ToolItemId": 261432
+    }
+}, {
+    "MaxPoints": 10,
+    "CanExceedMaxPoints": false,
+    "IsBonus": false,
+    "ExcludeFromFinalGradeCalculation": false,
+    "GradeSchemeId": null,
+    "GradeSchemeUrl": "/d2l/api/le/1.14/10011/grades/schemes/0",
+    "Id": 573064,
+    "Name": "CSVIn",
+    "ShortName": "",
+    "GradeType": "Numeric",
+    "CategoryId": 0,
+    "Description": {
+        "Text": "",
+        "Html": ""
+    },
+    "Weight": 10,
+    "AssociatedTool": null
+}];
